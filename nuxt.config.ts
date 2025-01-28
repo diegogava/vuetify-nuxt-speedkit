82f7e4ba2c512fb98a302e2213ebcd1dfacdaf5c
diegogava <dalmeidagava>
2025-01-28 16:25:10 -0300

    Install and set nuxt-vitalizer library

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,147 +9,129 @@ const image = "https://vuetify3nuxt3starter.behonbaker.com/starter.png"
 const url = "https://vuetify3nuxt3starter.behonbaker.com/"
 
 export default defineNuxtConfig({
-  app: {
-      head: {
+    app: {
+        head: {
         title: "Vuetify 3 + Nuxt 3 Starter",
         titleTemplate: "%s | Vuetify 3 + Nuxt 3 Starter",
         link: [
-          { rel: "stylesheet", href: "https://rsms.me/inter/inter.css" },
-          { rel: "preconnect", href: "https://rsms.me/" },
-          { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
-          { rel: "canonical", href: url },
+            { rel: "stylesheet", href: "https://rsms.me/inter/inter.css" },
+            { rel: "preconnect", href: "https://rsms.me/" },
+            { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+            { rel: "canonical", href: url },
         ],
         meta: [
-          {
+            {
             hid: "description",
             name: "description",
             content: description,
-          },
-          { property: "og:site_name", content: title },
-          { hid: "og:type", property: "og:type", content: "website" },
-          {
+            },
+            { property: "og:site_name", content: title },
+            { hid: "og:type", property: "og:type", content: "website" },
+            {
             hid: "og:url",
             property: "og:url",
             content: url,
-          },
-          {
+            },
+            {
             hid: "og:image:secure_url",
             property: "og:image:secure_url",
             content: image,
-          },
-          {
+            },
+            {
             hid: "og:title",
             property: "og:title",
             content: title,
-          },
-          {
+            },
+            {
             hid: "og:description",
             property: "og:description",
             content: description,
-          },
-          {
+            },
+            {
             hid: "og:image",
             property: "og:image",
             content: image,
-          },
-          //Twitter
-          { name: "twitter:card", content: "summary_large_image" },
-          {
+            },
+            //Twitter
+            { name: "twitter:card", content: "summary_large_image" },
+            {
             hid: "twitter:url",
             name: "twitter:url",
             content: url,
-          },
-          {
+            },
+            {
             hid: "twitter:title",
             name: "twitter:title",
             content: title,
-          },
-          {
+            },
+            {
             hid: "twitter:description",
             name: "twitter:description",
             content: description,
-          },
-          {
+            },
+            {
             hid: "twitter:image",
             name: "twitter:image",
             content: image,
-          },
+            },
         ],
-      },
-  },
-
-  build: { transpile: ["vuetify"] },
-
-  css: [
-      '@/styles/main.scss',
-      '@/styles/css/tailwind.css',
-  ],
-
-  devtools: { enabled: false },
-
-  experimental: {
-      renderJsonPayloads: true
-  },
+        },
+    },
+    build: { transpile: ["vuetify"] },
+    compatibilityDate: "2025-01-21",
+    css: [
+        '@/styles/main.scss',
+        '@/styles/css/tailwind.css',
+    ],
+    devtools: { enabled: false },
+    experimental: {
+        renderJsonPayloads: true
+    },
 
   modules: [
       '@nuxt/image',
-      'nuxt-booster',
       '@nuxtjs/tailwindcss',
-      async (options, nuxt) => {
-          nuxt.hooks.hook("vite:extendConfig", (config) =>
-              // @ts-ignore
-              config.plugins.push(vuetify())
-          );
-      },
-  ],
+      'nuxt-vitalizer',
+        async (options, nuxt) => {
+            nuxt.hooks.hook("vite:extendConfig", (config) => {
+                config?.plugins?.push(
+                    vuetify({
+                        autoImport: true, // Autoimport components (def true)
+                    }),
+                );
+            });
+        },
+        //   async (options, nuxt) => {
+        //       nuxt.hooks.hook("vite:extendConfig", (config) =>
+        //           // @ts-ignore
+        //           config.plugins.push(vuetify())
+        //       );
+        //   },
+    ],
 
-  nitro: {
-      compressPublicAssets: true,
-      //preset: process.env.ENV === 'production' ? 'vercel-edge' : null,
-      // prerender: {
-      //   routes: [
-      //     '/sitemap.xml',
-      //   ]
-      // },
-    //   routeRules: {
-    //     "/": { ssr: true, prerender: true },
-    //   }
-  },
-  booster: {
-
-    detection: {
-      performance: true,
-      browserSupport: true
+    nitro: {
+        compressPublicAssets: true,
     },
-
-    performanceMetrics: {
-      device: {
-        hardwareConcurrency: { min: 2, max: 48 },
-        deviceMemory: { min: 2 }
-      },
-      timing: {
-        fcp: 800,
-        dcl: 1200
-      }
+    ssr: true,
+    typescript: { shim: false, strict: true, },
+    vitalizer: {
+        disableStylesheets: 'entry'
     },
-
-    targetFormats: ['webp', 'avif', 'jpg|jpeg|png|gif'],
-
-    componentAutoImport: false,
-    componentPrefix: undefined,
-
-    /**
-     * IntersectionObserver rootMargin for Compoennts and Assets
-     */
-    lazyOffset: {
-      component: '0%',
-      asset: '0%'
-    }
-
-  },
-
-
-  ssr: true,
-  typescript: { shim: false, strict: true, },
-  compatibilityDate: "2025-01-21",
-});
+    vite: {
+        // build: {
+        // 	/* used minifier when transpiling (def "esbuild", false = disable)
+        // 	   https://vitejs.dev/config/build-options.html#build-minify */
+        // 	minify: "esbuild",
+        // 	/* Size reporting for gzip-compressed files.
+        // 		Disabling may increase build performance in large projects (def true)
+        // 		https://vitejs.dev/config/build-options.html#build-reportcompressedsize */
+        // 	reportCompressedSize: false,
+        // },
+        esbuild: {
+            drop: [
+                "debugger",
+                "console",
+            ]
+        },
+	},
+})
